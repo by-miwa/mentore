f1ffeb71f3f3e0354de8913c5c573aa26772e96d
by-miwa <miwa.boardgame@gmail.com>
2020-12-03 17:52:37 +0900

    エラーメッセージ日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,17 @@ class User < ApplicationRecord
 
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
          validates_format_of :password, with: PASSWORD_REGEX
-         validates :occupation_id, numericality: { other_than: 1 }
-         validates :study_id, numericality: { other_than: 1 }
-         validates :age_id, numericality: { other_than: 1 }
+         
          validates :name, presence: true
          validates :profile, presence: true
          validates :occupation, presence: true
          validates :study, presence: true
-         validates :sns, presence: true
          validates :age, presence: true
+         validates :sns, presence: true
+
+         validates :occupation_id, numericality: { other_than: 1 }
+         validates :study_id, numericality: { other_than: 1 }
+         validates :age_id, numericality: { other_than: 1 }
 
          has_many :prototypes
          has_many :comments
