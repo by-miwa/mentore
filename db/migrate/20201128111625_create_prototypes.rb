3d3952b655e83c0770fe27b8af79a91a95d78435
by-miwa <miwa.boardgame@gmail.com>
2020-12-03 11:06:45 +0900

    テーブル作成

--- db/migrate/20201128111625_create_prototypes.rb
+++ db/migrate/20201128111625_create_prototypes.rb
@@ -7,6 +7,7 @@ class CreatePrototypes < ActiveRecord::Migration[6.0]
       t.string :appli, null: false
       t.string :github, null: false     
       t.references :user, foreign_key: true
+      t.integer     :genre,  null: false           # ジャンル
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_054853) do
+ActiveRecord::Schema.define(version: 2020_12_03_020306) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2020_11_29_054853) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text", null: false
     t.bigint "user_id"
@@ -50,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_11_29_054853) do
     t.string "appli", null: false
     t.string "github", null: false
     t.bigint "user_id"
+    t.integer "genre", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_prototypes_on_user_id"
