487d8527b5bfd5148f4d31a18371cf43f2287c41
by-miwa <miwa.boardgame@gmail.com>
2020-12-03 16:14:56 +0900

    genreバリデーション

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -11,6 +11,7 @@ class Prototype < ApplicationRecord
   validates :concept, presence: true
   validates :appli, presence: true
   validates :github, presence: true
+  validates :genre_id, numericality: { other_than: 1 }
   validates :image, presence: true, unless: :was_attached?
 
   def was_attached?
