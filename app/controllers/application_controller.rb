e58596e695ce9ff266e6c08c1f2f36bf4a6f86d8
by-miwa <miwa.boardgame@gmail.com>
2020-12-08 11:48:17 +0900

    ゲストログイン コメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,31 +10,26 @@ class ApplicationController < ActionController::Base
     @results = @p.result.includes(:genre)  # 検索条件にマッチした商品の情報を取得
   end
   
-  def new_guest
-    user = User.find_or_create_by!(email: 'yamada@com') do |user|
-    user = User.find_or_create_by!(name: 'yamada') do |user|
-    user = User.find_or_create_by!(profile: 'よろしくお願い致します。') do |user|
-    user = User.find_or_create_by!(occupation_id: '2') do |user|
-    user = User.find_or_create_by!(study_id: '2') do |user|
-    user = User.find_or_create_by!(age_id: '2') do |user|
-    user = User.find_or_create_by!(sns: 'あ') do |user|
-    user.password = SecureRandom.urlsafe_base64
-    end
-    end
-    end
-    end
-    end
-    end
-    end
-    sign_in user
-    redirect_to root_path, notice: 'ゲストログインしました。'
-  end
 
-  private
-
-  def search_prototype
-    @p = Prototype.ransack(params[:q])  # 検索オブジェクトを生成
-  end
+  #def new_guest
+    #user = User.find_or_create_by!(email: 'yamada@com') do |user|
+    #user = User.find_or_create_by!(name: 'yamada') do |user|
+    #user = User.find_or_create_by!(profile: 'よろしくお願い致します。') do |user|
+    #user = User.find_or_create_by!(occupation_id: '2') do |user|
+    #user = User.find_or_create_by!(study_id: '2') do |user|
+    #user = User.find_or_create_by!(age_id: '2') do |user|
+    #user = User.find_or_create_by!(sns: 'あ') do |user|
+    #user.password = SecureRandom.urlsafe_base64
+    #end
+    #end
+    #end
+    #end
+    #end
+    #end
+    #end
+    #sign_in user
+    #redirect_to root_path, notice: 'ゲストログインしました。'
+  #end
 
   private
   def configure_permitted_parameters
