1dee88577405313cd10364442931a3c63da12bed
by-miwa <miwa.boardgame@gmail.com>
2020-12-08 11:51:38 +0900

    検索アクション削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :search_prototype
 
   def index
     @prototypes = Prototypes.all  # 全商品の情報を取得
